be5001b5d635b4f6d56ee32155b6bcfeccb6f4cd
Justin King <justin.king@jdmsolutions.com>
2024-08-24 08:33:14 -0500

    initial check in

---
+++ Ollama-Blazor/Data/OllamaApiSettings.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Data
+{
+    public class OllamaApiSettings
+    {
+        public string BaseUrl { get; set; } = "localhost:11434";
+        public string EmbeddingsModel { get; set; } = "mxbai-embed-large";
+        public string LLMModel { get; set; } = "mistral:latest";
+    }
+}
---
+++ Ollama-Blazor/Program.cs
@@ -0,0 +1,59 @@
+using Data;
+using Markdig;
+using Markdown.ColorCode;
+using Microsoft.Extensions.Options;
+using Ollama_Blazor.Components;
+using OllamaSharp;
+
+namespace Ollama_Blazor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            var pipeline = new MarkdownPipelineBuilder()
+                    .UseAdvancedExtensions()
+                    .UseColorCode()
+                    .Build();
+
+            builder.Services.AddSingleton(pipeline);
+
+
+            builder.Services.Configure<OllamaApiSettings>(builder.Configuration.GetSection("OllamaApi"));
+            builder.Services.AddScoped<IOllamaApiClient>(sp =>
+            {
+                var config = sp.GetRequiredService<IOptions<OllamaApiSettings>>().Value;
+
+                return new OllamaApiClient(new Uri(config.BaseUrl));
+            });
+
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
